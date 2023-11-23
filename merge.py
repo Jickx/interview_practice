ce106f4d957be6e68c035eb7bcfa1e6047c7eab1
Jickx <valentin.sharonov@gmail.com>
2023-11-23 20:14:52 +0300

    refactor

--- merge.py
+++ merge.py
@@ -1,11 +1,8 @@
 def merge(lst1: list, lst2: list) -> dict:
-    result = {}
-    for i in range(len(lst1)):
-        result[lst1[i]] = lst2[i]
-    return dict(sorted(result.items()))
+    return dict(sorted(zip(lst1, lst2)))
 
 
-lst1 = [3, 2, 1]
-lst2 = [4, 5, 6]
+lst1 = [2, 3, 1]
+lst2 = ['a', 'b', 'c']
 
 print(merge(lst1, lst2))
