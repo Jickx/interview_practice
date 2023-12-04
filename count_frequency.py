7640e23664bc9bb5a67e6b424c1849826005ef3e
Jickx <valentin.sharonov@gmail.com>
2023-12-04 21:26:49 +0300

    without Counter

--- count_frequency.py
+++ count_frequency.py
@@ -1,8 +1,9 @@
-from collections import Counter
-
-
 def count_frequency(seq):
-    return Counter(seq)
+    seq_set = set(seq)
+    counter_dict = {x: 0 for x in seq_set}
+    for i in seq:
+        counter_dict[i] += 1
+    return counter_dict
 
 
 seq = [2, 3, 2, 4, 5, 6, 4, 5, 2, 4, 4, 4]
