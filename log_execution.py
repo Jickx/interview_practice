248da0c72ca5482ba7c89c344952f8a1a5dbf7fd
Jickx <valentin.sharonov@gmail.com>
2023-12-04 22:08:10 +0300

    add log_execution.py

---
+++ log_execution.py
@@ -0,0 +1,19 @@
+def log_execution(func):
+    def wrapper(a, b):
+        print(f'Executing function: {func.__name__}')
+        print(f'Arguments: {a, b}')
+        return func(a, b)
+    return wrapper
+
+
+@log_execution
+def add_numbers(a, b):
+    return a + b
+
+# Test the decorator
+result = add_numbers(3, 5)
+print("Result:", result)
+
+# Executing function: add_numbers
+# Arguments: (3, 5)
+# Result: 8
